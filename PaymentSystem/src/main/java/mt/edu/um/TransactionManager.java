24867f1a12ed0a895c41bbbaee990621a8ca0391
4jzbra <zeraj506@yahoo.com>
2015-03-31 17:26:07 +0200

    Class update

--- PaymentSystem/src/main/java/mt/edu/um/TransactionManager.java
+++ PaymentSystem/src/main/java/mt/edu/um/TransactionManager.java
@@ -4,6 +4,8 @@ public class TransactionManager {
 	
 	private int numTransactionsProcessed;
 	
-	public boolean processTransaction(int src)
+	public boolean processTransaction(int src){
+		return true;
+	}
 
 }
