21c5cbc2d3c04c13f4da6defd71d0f67ddd7c764
Samuel <mizzisamuel@gmail.com>
2015-03-31 18:51:23 +0200

    updates

--- PaymentSystem/src/main/java/mt/edu/um/Account.java
+++ PaymentSystem/src/main/java/mt/edu/um/Account.java
@@ -6,43 +6,43 @@ public class Account {
 	private String accountName;  // eg fixed account, savings, etc...
 	private long accountBalance; // current amount in the account
 	
-	public Account (){      //default constructor
+	public Account(){      //default constructor
 		accountNumber = 0;
 		accountName = null;
 		accountBalance = 0;
 	}
 	
-	public Account (int accNo, String accName, long accBal){
-		setAccountNumber (accNo);
+	public Account(int accNo, String accName, long accBal){
+		setAccountNumber(accNo);
 		accountName = accName;
 		accountBalance = accBal;
 	}
 	
-	public boolean adjustBalance (long amount){   // checks if balance is correct
+	public boolean adjustBalance(long amount){   // checks if balance is correct
 		return false;
 	}
 	
-	public void setAccountNumber (int accNo){
+	public void setAccountNumber(int accNo){
 		accNo = accountNumber;
 	}
 	
-	public void setAccountName (String accName){
+	public void setAccountName(String accName){
 		accName = accountName;
 	}
 	
-	public void setAccountBalance (long accBal){
+	public void setAccountBalance(long accBal){
 		accBal = accountBalance;
 	}
 	
-	int getAccountNumber (){
+	int getAccountNumber(){
 		return accountNumber;
 	}
 	
-	String getAccountName (){
+	String getAccountName(){
 		return accountName;
 	}
 	
-	long getAccountBalance (){
+	long getAccountBalance(){
 		return accountBalance;
 	}
 	
--- PaymentSystem/src/main/java/mt/edu/um/AccountDatabase.java
+++ PaymentSystem/src/main/java/mt/edu/um/AccountDatabase.java
@@ -1,10 +1,22 @@
 package mt.edu.um;
 
-public class AccountDatabase {
+public class AccountDatabase extends Account {
+	
+	public AccountDatabase(){
+		super();
+	}
+	
+	public AccountDatabase(int accNo, String accName, long accBal){
+		super(accNo, accName, accBal);
+	}
 	
 	public Account getAccount (int accountNumber){
 		Account acc = new Account();
 		return acc;
+	}
+	
+	public int getSize(){
+		return 0;
 	}
 
 }
