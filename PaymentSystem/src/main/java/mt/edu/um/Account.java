926b857eeea631060994dda19cec081230025516
Samuel <mizzisamuel@gmail.com>
2015-03-31 18:05:04 +0200

    Account update

--- PaymentSystem/src/main/java/mt/edu/um/Account.java
+++ PaymentSystem/src/main/java/mt/edu/um/Account.java
@@ -2,20 +2,24 @@ package mt.edu.um;
 
 public class Account {
 	
-	private int accountNumber;
-	private String accountName;
-	private long accountBalance;
+	private int accountNumber;   // unique account number
+	private String accountName;  // eg fixed account, savings, etc...
+	private long accountBalance; // current amount in the account
 	
-	public Account(){
+	public Account (){      //default constructor
 		accountNumber = 0;
 		accountName = null;
 		accountBalance = 0;
 	}
 	
-	public Account(int accNo, String accName, long accBal){
+	public Account (int accNo, String accName, long accBal){
 		setAccountNumber (accNo);
 		accountName = accName;
 		accountBalance = accBal;
+	}
+	
+	public boolean adjustBalance (long amount){   // checks if balance is correct
+		return false;
 	}
 	
 	public void setAccountNumber (int accNo){
